7c95cc81f43e48108c6a3584b89607782b3255dc
Aminatakabba <aminata-kabba@hotmail.com>
2020-08-28 23:57:44 +0100

    firebase errors fixed

--- instagram-clone/src/firebase.js
+++ instagram-clone/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAxIpTT1wjRKRC5VTdpdS7hrKtkNuo_G0A",
-    authDomain: "instagram-clone-952ba.firebaseapp.com",
-    databaseURL: "https://instagram-clone-952ba.firebaseio.com",
-    projectId: "instagram-clone-952ba",
-    storageBucket: "instagram-clone-952ba.appspot.com",
-    messagingSenderId: "427158021204",
-    appId: "1:427158021204:web:4896bb2ba4e0505247f9c1",
-    measurementId: "G-SVM78K8GBE"
+    apiKey: "AIzaSyDDYz7FNvstjskPw8NmHvIiAWYRW0aeMKs",
+    authDomain: "instagram-clone-react-6703c.firebaseapp.com",
+    databaseURL: "https://instagram-clone-react-6703c.firebaseio.com",
+    projectId: "instagram-clone-react-6703c",
+    storageBucket: "instagram-clone-react-6703c.appspot.com",
+    messagingSenderId: "748899887451",
+    appId: "1:748899887451:web:3348b6fc747496c72fd27e",
+    measurementId: "G-67L2G2FQPB"
 });
 
 const db = firebaseApp.firestore();
