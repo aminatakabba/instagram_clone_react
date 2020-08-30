f74d713a96d1fd0e3b39b1365221b9f522687843
Aminatakabba <aminata-kabba@hotmail.com>
2020-08-30 19:58:44 +0100

    image upload finished and styled

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -5,6 +5,8 @@ import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
+import './ImageUpload';
+import ImageUpload from './ImageUpload';
 
 function getModalStyle() {
   const top = 50;
@@ -55,7 +57,7 @@ function App() {
   }, [user, username])
  
   useEffect(() => {
-     db.collection('posts').onSnapshot(snapshot => {
+     db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
        setPosts(snapshot.docs.map(doc => ({ 
          id: doc.id,
          post: doc.data(), 
@@ -90,7 +92,7 @@ function App() {
    }
 
   return (
-    <div className="app">   
+    <div className="app">  
       <Modal
         open={open}
         onClose={() => setOpen(false)}>
@@ -151,31 +153,36 @@ function App() {
                   onChange={(e) => setPassword(e.target.value)}
               />
 
-              <Button type="submit" onClick={signUp}>Sign In</Button>
+              <Button type="submit" onClick={signIn}>Sign In</Button>
             </form>
           </div>
       </Modal>
-
+      
+      
       <div className="app_header">
       <img className="app_headerImage" 
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" 
             alt="" />
+          {user ? (
+            <Button onClick={() => auth.signOut()}>Log out</Button>
+          ): (
+            <div className="app__loginContainer">
+              <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+              <Button onClick={() => setOpen(true)}>Sign Up</Button>
+            </div>
+          )}
       </div>
-
-      {user ? (
-        <Button onClick={() => auth.signOut()}>Log out</Button>
-      ): (
-        <div className="app__loginContainer">
-          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
-          <Button onClick={() => setOpen(true)}>Sign Up</Button>
-        </div>
-      )}
-
+      
       {
         posts.map(({id, post}) => (
           <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
         ))
       }
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName}/>
+      ): (
+        <h3>Sorry you need to login to upload</h3>
+      )}
 
     </div>
   );
